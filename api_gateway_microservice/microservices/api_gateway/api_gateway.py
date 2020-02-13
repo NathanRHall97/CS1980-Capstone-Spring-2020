ef8d50c9f8de447a5e937b688b3f46c1528fa1c3
Nathan Hall <nrh20@pitt.edu>
2020-02-12 23:52:29 -0500

    Edited 'gateway.mutache' to make a clean api_gateway file that works when build.py is ran

--- api_gateway_microservice/microservices/api_gateway/api_gateway.py
+++ api_gateway_microservice/microservices/api_gateway/api_gateway.py
@@ -7,49 +7,47 @@ app = Flask(__name__)
 def get_index():
     return 'You have reached the API-Gateway index page'
 
+
 @app.route('/ui', methods=['get'])
 def ui():
-    microservice_response = requests.get("http://172.16.238.5:8080/ui",
-        
-    )
+    content = request.get_json()
+    microservice_response = requests.get("http://172.16.238.5:8080/ui".format(), data=content)
     return microservice_response.content
 @app.route('/ui-yaml', methods=['get'])
 def ui_yaml():
-    microservice_response = requests.get("http://172.16.238.5:8080/ui-yaml",
-        
-    )
+    content = request.get_json()
+    microservice_response = requests.get("http://172.16.238.5:8080/ui-yaml".format(), data=content)
     return microservice_response.content
 @app.route('/user', methods=['post'])
 def register_customer():
     content = request.get_json()
-    microservice_response = requests.post("http://172.16.238.7:8080/user", data=content)
+    microservice_response = requests.post("http://172.16.238.7:8080/user".format(), data=content)
     return microservice_response.content
 @app.route('/user', methods=['get'])
 def get():
-    microservice_response = requests.get("http://172.16.238.7:8080/user",
-    )
+    content = request.get_json()
+    microservice_response = requests.get("http://172.16.238.7:8080/user".format(), data=content)
     return microservice_response.content
 @app.route('/user/<customerId>', methods=['get'])
-def get_user(customerId,):
-    microservice_response = requests.get("http://172.16.238.7:8080/user/{}".format(customerId)
-    )
+def get_user(customerId):
+    content = request.get_json()
+    microservice_response = requests.get("http://172.16.238.7:8080/user/{}".format(customerId), data=content)
     return microservice_response.content
 @app.route('/pet', methods=['post'])
 def update_pet():
     content = request.get_json()
-    microservice_response = requests.post("http://172.16.238.8:8080/pet", data=content)
+    microservice_response = requests.post("http://172.16.238.8:8080/pet".format(), data=content)
     return microservice_response.content
 @app.route('/pet', methods=['get'])
 def update_pets():
-    microservice_response = requests.get("http://172.16.238.8:8080/pet",
-        
-    )
+    content = request.get_json()
+    microservice_response = requests.get("http://172.16.238.8:8080/pet".format(), data=content)
     return microservice_response.content
 @app.route('/pet/<petId>', methods=['get'])
-def find_by_id(petId,):
-    microservice_response = requests.get("http://172.16.238.8:8080/pet/{}".format(petId)
-    )
+def find_by_id(petId):
+    content = request.get_json()
+    microservice_response = requests.get("http://172.16.238.8:8080/pet/{}".format(petId), data=content)
     return microservice_response.content
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
