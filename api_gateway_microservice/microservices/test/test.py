65c1669778c6e34662d5d5d34af6c0a01cc6347b
Nathan Hall <nrh20@pitt.edu>
2020-03-11 13:16:37 -0400

    outputting test calls

--- api_gateway_microservice/microservices/test/test.py
+++ api_gateway_microservice/microservices/test/test.py
@@ -16,9 +16,10 @@ def test_get_pets():
     url = "http://172.16.238.7:8080/pet"
     # Send a GET request
     response = requests.get(url)
-    print(response)
+    print(response.json())
     assert response.status_code == 200
 
+#CREATE PET
 def test_post_pet():
     mimetype = 'application/json'
     headers = {
@@ -30,16 +31,18 @@ def test_post_pet():
     content = {"id": 0, "name": "gizmo",  "status": "available", "species": "dog", "subspecies": "lab"}
 
     response = requests.post(url, data = json.dumps(content), headers = headers)
-
+    print(response.json())
     assert response.status_code == 200
 
+#READ PET
 def test_get_pet():
     url = "http://172.16.238.7:8080/pet/0"
     # Send a GET request
     response = requests.get(url)
-    print(response)
+    print(response.json())
     assert response.status_code == 200
 
+#UPDATE PET
 def test_patch_pet():
     mimetype = 'application/json'
     headers = {
@@ -49,23 +52,26 @@ def test_patch_pet():
     url = "http://172.16.238.7:8080/pet"
     content = {"id": 0, "name": "gizmo",  "status": "SOLD", "species": "dog", "subspecies": "lab"}
     response = requests.patch(url, data = json.dumps(content), headers = headers)
-    print(response)
+    print(response.json())
     assert response.status_code == 200
 
+#DELETE PET
 def test_delete_pet():
     url = "http://172.16.238.7:8080/pet/0"
     # Send a GET request
     response = requests.delete(url)
-    print(response)
+    print(response.json())
     assert response.status_code == 200
+
 
 def test_get_users():
     url = "http://172.16.238.7:8080/user"
     # Send a GET request
     response = requests.get(url)
-    print(response)
+    print(response.json())
     assert response.status_code == 200
 
+#CREATE USER
 def test_post_user():
     mimetype = 'application/json'
     headers = {
@@ -77,16 +83,18 @@ def test_post_user():
     content = {"id": 0, "name": "jerry", "role": "Customer"}
 
     response = requests.post(url, data = json.dumps(content), headers = headers)
-
+    print(response.json())
     assert response.status_code == 200
 
+#READ USER
 def test_get_user():
     url = "http://172.16.238.7:8080/user/0"
     # Send a GET request
     response = requests.get(url)
-    print(response)
+    print(response.json())
     assert response.status_code == 200
 
+#UPDATE USER
 def test_patch_user():
     mimetype = 'application/json'
     headers = {
@@ -96,12 +104,13 @@ def test_patch_user():
     url = "http://172.16.238.7:8080/user"
     content = {"id": 0, "name": "jerry", "role": "BANNED"}
     response = requests.patch(url, data = json.dumps(content), headers = headers)
-    print(response)
+    print(response.json())
     assert response.status_code == 200
 
+#DELETE USER
 def test_delete_user():
     url = "http://172.16.238.7:8080/user/0"
     # Send a GET request
     response = requests.delete(url)
-    print(response)
+    print(response.json())
     assert response.status_code == 200
