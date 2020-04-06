53dccfda1d645f6bbbc3d2214063b956721264dd
shonjh <42815732+shonjh@users.noreply.github.com>
2020-04-06 10:50:57 -0400

    Added tests for User.py

--- api_gateway_microservice/microservices/user/test/test_user.py
+++ api_gateway_microservice/microservices/user/test/test_user.py
@@ -53,3 +53,62 @@ def test_delete_user():
     response = requests.delete(url)
     print(response.json())
     assert response.status_code == 200
+
+# Tests to see if a negative integer will throw an error. In user.py user_len(id) must be > 0.
+def test_negative_id_user():
+    url = "http://172.16.238.7:8080/user/-1"
+    response = requests.get(url)
+    print(response.json())
+    assert response.status_code == 404
+
+# added edge case test (USER)
+# Tests to see if a negative integer will delete or modify anything. In user.py user_len(id) must be > 0.
+def test_negative_id_delete_user():
+    url = "http://172.16.238.7:8080/user/-1"
+    # Send a GET request
+    response = requests.delete(url)
+    print(response.json())
+    assert response.status_code == 404
+
+
+# added test (USER)
+# Makes sure that a header is necessary and throws a 405 error without one.
+def test_header_user():
+    mimetype = 'application/json'
+    headers = { }
+    url = "http://172.16.238.7:8080/user"
+    content = {"id": 0, "name": "jerry", "role": "Customer"}
+
+    response = requests.post(url, data=json.dumps(content), headers=headers)
+    print(response.json())
+    assert response.status_code == 405
+
+# added test (USER)
+# Makes sure that a url is necessary and throws a 405 error without one.
+def test_url_user():
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype
+    }
+
+    url = ""
+    content = {"id": 0, "name": "jerry", "role": "Customer"}
+
+    response = requests.post(url, data = json.dumps(content), headers = headers)
+    print(response.json())
+    assert response.status_code == 200
+# added test (USER)
+# Makes sure that content is necessary and throws a 405 error without one.
+def test_content_user():
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype
+    }
+    url = ""
+    content = {}
+
+    response = requests.post(url, data=json.dumps(content), headers=headers)
+    print(response.json())
+    assert response.status_code == 200
